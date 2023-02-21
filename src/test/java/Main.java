506417a35b677e82189e0a37709422aad9404918
kerauns <vesstar.dj@gmail.com>
2023-02-21 16:39:56 +0300

    changed 4 line

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello wlrld");
-        System.out.println("test push2fork");
+        System.out.println("testjyguyvurk");
     }
 
 }
